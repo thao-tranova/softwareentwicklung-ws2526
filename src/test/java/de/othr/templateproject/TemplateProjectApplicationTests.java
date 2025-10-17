b5ac3f17cf8942b08e7a2f81e4c5450f6ef6eef2
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-10-17 16:11:34 +0200

    Template project with dependencies to - Spring Web - Spring Boot DevTools - Thymeleaf

---
+++ src/test/java/de/othr/templateproject/TemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.templateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
