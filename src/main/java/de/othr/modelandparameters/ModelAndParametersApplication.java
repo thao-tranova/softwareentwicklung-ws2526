1e32d23a192ef622d4425020f68da42e2509780c
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-10-18 17:22:11 +0200

    Startprojekt

--- src/main/java/de/othr/modelandparameters/ModelAndParametersApplication.java
+++ src/main/java/de/othr/modelandparameters/ModelAndParametersApplication.java
@@ -1,13 +1,13 @@
-package de.othr.templateproject;
+package de.othr.modelandparameters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateProjectApplication {
+public class ModelAndParametersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TemplateProjectApplication.class, args);
+        SpringApplication.run(ModelAndParametersApplication.class, args);
     }
 
 }
--- src/test/java/de/othr/modelandparameters/ModelAndParametersApplicationTests.java
+++ src/test/java/de/othr/modelandparameters/ModelAndParametersApplicationTests.java
@@ -1,10 +1,10 @@
-package de.othr.templateproject;
+package de.othr.modelandparameters;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TemplateProjectApplicationTests {
+class ModelAndParametersApplicationTests {
 
     @Test
     void contextLoads() {
