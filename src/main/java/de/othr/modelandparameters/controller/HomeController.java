c209c15e0bac7941fcfc3d9b04222d759840066f
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-10-18 18:40:34 +0200

    Aufgabe 3

--- src/main/java/de/othr/modelandparameters/controller/HomeController.java
+++ src/main/java/de/othr/modelandparameters/controller/HomeController.java
@@ -1,8 +1,11 @@
 package de.othr.modelandparameters.controller;
 
+import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,6 +31,21 @@ public class HomeController {
         System.out.println("password: " + params.get("password"));
         System.out.println("role: " + params.get("role"));
         return "login-processed";
+    }
+
+    // Aufgabe 3
+    @GetMapping("/pathvariable")
+    public String showPathVariablePage(Model model) {
+        List<String> postids = List.of("1", "2", "3", "4", "5");
+        model.addAttribute("postids", postids);
+        return "pathvariable";
+    }
+
+    @RequestMapping(value = "/pathvariable/process/{postid}")
+    public String getMethodName(@PathVariable String postid, Model model) {
+        System.out.println("Showing the POST "+ postid);
+        model.addAttribute("postid", postid);
+        return "pathvariable-processed";
     }
 }
 
