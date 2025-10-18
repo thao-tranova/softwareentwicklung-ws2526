e853895036dc8d7ca1d55dc82e7677a031182b49
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-10-18 17:58:48 +0200

    Aufgabe 2

--- src/main/java/de/othr/modelandparameters/controller/HomeController.java
+++ src/main/java/de/othr/modelandparameters/controller/HomeController.java
@@ -1,5 +1,6 @@
 package de.othr.modelandparameters.controller;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,21 @@ public class HomeController {
     public String showLoginForm() {
         return "login";
     }
+
+    // Aufgabe 1
     @RequestMapping("/login/process")
     public String processLoginForm(@RequestParam String user, @RequestParam String password) {
         System.out.println("username: " + user);
         System.out.println("password: " + password);
+        return "login-processed";
+    }
+
+    // Aufgabe 2
+    @RequestMapping("/login/processMap")
+    public String processMapParamForm(@RequestParam Map<String, String> params) {
+        System.out.println("username: " + params.get("user"));
+        System.out.println("password: " + params.get("password"));
+        System.out.println("role: " + params.get("role"));
         return "login-processed";
     }
 }
