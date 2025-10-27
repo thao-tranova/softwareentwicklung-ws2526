24827734b79e696740ecb147f2d134ee4c70f49e
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-10-27 09:13:12 +0100

    Startprojekt

--- src/main/java/de/othr/validation/ValidationApplication.java
+++ src/main/java/de/othr/validation/ValidationApplication.java
@@ -1,13 +1,13 @@
-package de.othr.templateproject;
+package de.othr.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateProjectApplication {
+public class ValidationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TemplateProjectApplication.class, args);
+        SpringApplication.run(ValidationApplication.class, args);
     }
 
 }
