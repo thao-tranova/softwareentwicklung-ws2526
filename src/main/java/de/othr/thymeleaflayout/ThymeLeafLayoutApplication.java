6133058a1c518e729d290f78e91afbb5320e2bb3
Thao Tranová <thao.tranova@st.oth-regensburg.de>
2025-11-09 13:28:16 +0100

    Aufgabe 1.2

---
+++ src/main/java/de/othr/thymeleaflayout/ThymeLeafLayoutApplication.java
@@ -0,0 +1,13 @@
+package de.othr.thymeleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/othr/thymeleaflayout/ThymeLeafLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.thymeleaflayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafLayoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
